31c694209b2b77f248dcea8cf3c5ee0737437813
denisnumb <denchik548@gmail.com>
2026-05-21 23:56:23 +0500

    fix: sendMessageToAllPlayersFromPlayer - Do not modify the passed content argument

--- common/src/main/java/com/denisnumb/discord_chat_mod/MinecraftUtils.java
+++ common/src/main/java/com/denisnumb/discord_chat_mod/MinecraftUtils.java
@@ -122,7 +122,7 @@ public class MinecraftUtils {
         if (playerList == null)
             return;
 
-        content = MinecraftEvents.handleChatMessage(
+        Component preparedContent = MinecraftEvents.handleChatMessage(
                 CustomChatTypeRegistry.CHAT,
                 new MinecraftChatStyleProvider.ChatMessageComponents(player.getDisplayName(), content, null, player)
         ).orElse(
@@ -133,7 +133,7 @@ public class MinecraftUtils {
         );
 
         playerList.broadcastChatMessage(
-                PlayerChatMessage.unsigned(player.getUUID(), content.getString()).withUnsignedContent(content),
+                PlayerChatMessage.unsigned(player.getUUID(), content.getString()).withUnsignedContent(preparedContent),
                 player,
                 CustomChatTypeRegistry.buildBound(
                         CustomChatTypeRegistry.CHAT,
