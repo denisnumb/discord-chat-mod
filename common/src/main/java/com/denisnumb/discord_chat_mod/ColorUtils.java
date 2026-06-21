a5a94475b4f6808a59aba9fac39180b5eb4e325c
denisnumb <denchik548@gmail.com>
2026-06-21 20:35:22 +0500

    feat: Discord embeds support and image display algorithm rework

--- common/src/main/java/com/denisnumb/discord_chat_mod/ColorUtils.java
+++ common/src/main/java/com/denisnumb/discord_chat_mod/ColorUtils.java
@@ -7,9 +7,11 @@ import java.util.HashMap;
 
 public class ColorUtils {
     public static class Color{
+        public static final int TRANSPARENT_IMAGE_TAG_COLOR = -0xffffff;
         public static final int CHANNEL_MENTION_COLOR = 0x6974c9;
         public static final int DISCORD_RED_COLOR = 0xE74C3C;
         public static final int DISCORD_GREEN_COLOR = 0x2ECC71;
+        public static final int DISCORD_DEFAULT_COLOR = 0x3b3b41;
         public static int CHAT_LINK_COLOR = 0x00b7ff;
     }
 
---
+++ common/src/main/java/com/denisnumb/discord_chat_mod/mixin/helper/DrawingBackgroundGraphicsAccessMixin.java
@@ -0,0 +1,20 @@
+package com.denisnumb.discord_chat_mod.mixin.helper;
+
+import com.denisnumb.discord_chat_mod.ColorUtils;
+import net.minecraft.util.ARGB;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(targets = "net.minecraft.client.gui.components.ChatComponent$DrawingBackgroundGraphicsAccess")
+public abstract class DrawingBackgroundGraphicsAccessMixin {
+    @Redirect(
+            method = "handleTag",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/util/ARGB;color(FI)I")
+    )
+    private int skipTransparentTagColor(float alpha, int color) {
+        if (color == ColorUtils.Color.TRANSPARENT_IMAGE_TAG_COLOR)
+            return 0;
+        return ARGB.color(alpha, color);
+    }
+}
