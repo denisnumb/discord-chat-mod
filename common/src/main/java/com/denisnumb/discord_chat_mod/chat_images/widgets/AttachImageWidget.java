8423b4b57ee06b1d1c8f8ca905e5512c3883cda0
denisnumb <denchik548@gmail.com>
2026-08-29 21:02:10 +0500

    fix: don't set focus to AttachImageWidget

--- common/src/main/java/com/denisnumb/discord_chat_mod/chat_images/widgets/AttachImageWidget.java
+++ common/src/main/java/com/denisnumb/discord_chat_mod/chat_images/widgets/AttachImageWidget.java
@@ -1,10 +1,12 @@
 package com.denisnumb.discord_chat_mod.chat_images.widgets;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ComponentPath;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
+import net.minecraft.client.gui.navigation.FocusNavigationEvent;
 import net.minecraft.network.chat.Component;
 import org.jetbrains.annotations.NotNull;
 
@@ -19,6 +21,7 @@ public class AttachImageWidget extends AbstractWidget {
     public AttachImageWidget(int x, int y, int width, int height, Consumer<AttachImageWidget> onPress) {
         super(x, y, width, height, Component.literal("📎"));
         this.onPress = onPress;
+        this.setFocused(false);
         this.setTooltip(Tooltip.create(Component.literal(getTranslateClient(ATTACH_IMAGE))));
     }
 
@@ -40,6 +43,11 @@ public class AttachImageWidget extends AbstractWidget {
     @Override
     public void onClick(double d, double e) {
         onPress.accept(this);
+    }
+
+    @Override
+    public ComponentPath nextFocusPath(@NotNull FocusNavigationEvent event) {
+        return null;
     }
 
     @Override
